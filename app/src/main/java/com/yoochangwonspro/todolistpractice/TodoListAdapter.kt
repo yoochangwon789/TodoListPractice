24ad2e6c51e88f94d08075941b0ea0a8b528e77f
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-18 17:50:46 +0900

    Feat : TodoListAdapter class 의 ListAdapter 상속 및 초기 함수 셋업

--- app/src/main/java/com/yoochangwonspro/todolistpractice/TodoListAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/todolistpractice/TodoListAdapter.kt
@@ -1,4 +1,36 @@
 package com.yoochangwonspro.todolistpractice
 
-class TodoListAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.yoochangwonspro.todolistpractice.databinding.TodoListItemBinding
+import com.yoochangwonspro.todolistpractice.todomodel.TodoListModel
+
+class TodoListAdapter : ListAdapter<TodoListModel, TodoListAdapter.ViewHolder>(diffUtil) {
+
+    inner class ViewHolder(private val binding: TodoListItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    companion object {
+        val diffUtil = object : DiffUtil.ItemCallback<TodoListModel>() {
+            override fun areItemsTheSame(oldItem: TodoListModel, newItem: TodoListModel): Boolean {
+                TODO("Not yet implemented")
+            }
+
+            override fun areContentsTheSame(oldItem: TodoListModel, newItem: TodoListModel, ): Boolean {
+                TODO("Not yet implemented")
+            }
+
+        }
+    }
 }
