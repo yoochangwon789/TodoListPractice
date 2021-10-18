97812035f2d2f7a5b9c05c653a8aa455d1d0b7e2
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-18 19:26:10 +0900

    Feat : TodoListModel completeTodoList 파라미터 추가

--- app/src/main/java/com/yoochangwonspro/todolistpractice/todomodel/TodoListModel.kt
+++ app/src/main/java/com/yoochangwonspro/todolistpractice/todomodel/TodoListModel.kt
@@ -1,5 +1,6 @@
 package com.yoochangwonspro.todolistpractice.todomodel
 
 data class TodoListModel(
-    val itemName: String
+    val itemName: String,
+    val completeTodoList: Boolean = false
 )
