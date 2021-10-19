9c766dbc2a2cd40b1766920682b1e5c50798103b
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-19 22:35:27 +0900

    Feat : 회원 가입 기능 구현

--- app/src/main/java/com/yoochangwonspro/todolistpractice/LoginActivity.kt
+++ app/src/main/java/com/yoochangwonspro/todolistpractice/LoginActivity.kt
@@ -1,7 +1,10 @@
 package com.yoochangwonspro.todolistpractice
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -21,15 +24,27 @@ class LoginActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
+        initSignUpButton()
+    }
+
+    private fun initSignUpButton() {
+        binding.LoginSignUpButton.setOnClickListener {
+            signUp()
+        }
+    }
+
+    private fun signUp() {
         val email = binding.LoginEmailEditText.text.toString()
         val password = binding.LoginPasswordEditText.text.toString()
 
         auth.createUserWithEmailAndPassword(email, password)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-
+                    Toast.makeText(this, "회원 가입에 성공하셨습니다.", Toast.LENGTH_SHORT).show()
                 } else {
-
+                    Toast.makeText(this,
+                        "회원 가입에 실패하셨습니다. 이미 가입한 이메일이 존재할 수 있습니다.",
+                        Toast.LENGTH_SHORT).show()
                 }
             }
     }
