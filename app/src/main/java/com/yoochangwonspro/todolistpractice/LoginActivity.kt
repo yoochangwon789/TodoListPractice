2bbe14fa79799be8487262f3cc49a440420d1d16
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-19 18:46:21 +0900

    Feat : LoginActivity 의 FirebaseAuth 전역 프로퍼티 추가

--- app/src/main/java/com/yoochangwonspro/todolistpractice/LoginActivity.kt
+++ app/src/main/java/com/yoochangwonspro/todolistpractice/LoginActivity.kt
@@ -2,8 +2,16 @@ package com.yoochangwonspro.todolistpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginActivity : AppCompatActivity() {
+
+    private val auth: FirebaseAuth by lazy {
+        Firebase.auth
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
