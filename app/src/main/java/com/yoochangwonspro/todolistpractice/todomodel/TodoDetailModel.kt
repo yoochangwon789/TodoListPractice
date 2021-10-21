e35942cd27f24223bd4f7747d7d444b83a226ddb
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-21 20:21:54 +0900

    Feat : TodoDetailModel data 클래스로 전화 후 Entity 추가 작업

--- app/src/main/java/com/yoochangwonspro/todolistpractice/todomodel/TodoDetailModel.kt
+++ app/src/main/java/com/yoochangwonspro/todolistpractice/todomodel/TodoDetailModel.kt
@@ -1,4 +1,11 @@
 package com.yoochangwonspro.todolistpractice.todomodel
 
-class TodoDetailModel {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class TodoDetailModel(
+    @PrimaryKey val id: Int,
+    @ColumnInfo(name = "item_detail") val itemDetail: String?
+)
