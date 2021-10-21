53863da35325c8f2cbc646d8162bd9f76f745067
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-21 18:39:59 +0900

    Feat : LiveData 모델 적용 및 Firebase 실시간 업데이트로 구성 변경 완료

--- app/src/main/java/com/yoochangwonspro/todolistpractice/TodoListAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/todolistpractice/TodoListAdapter.kt
@@ -4,16 +4,14 @@ import android.graphics.Paint
 import android.graphics.Typeface
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.DiffUtil
-import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.google.firebase.firestore.DocumentSnapshot
 import com.yoochangwonspro.todolistpractice.databinding.TodoListItemBinding
-import com.yoochangwonspro.todolistpractice.todomodel.TodoListModel
 
 class TodoListAdapter(
-    private val todoListData: MutableList<TodoListModel>,
-    val itemDeleteClicked: (TodoListModel) -> Unit,
-    val itemCompleteClicked: (TodoListModel) -> Unit
+    private var todoListData: List<DocumentSnapshot>,
+    val itemDeleteClicked: (DocumentSnapshot) -> Unit,
+    val itemCompleteClicked: (DocumentSnapshot) -> Unit
 ) : RecyclerView.Adapter<TodoListAdapter.ViewHolder>() {
 
     class ViewHolder(val binding: TodoListItemBinding) :
@@ -27,7 +25,7 @@ class TodoListAdapter(
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val todoData = todoListData[position]
-        holder.binding.itemTodoTextView.text = todoData.itemName
+        holder.binding.itemTodoTextView.text = todoData.getString("itemName") ?: ""
 
         holder.binding.deleteImageButton.setOnClickListener {
             itemDeleteClicked(todoData)
@@ -37,7 +35,7 @@ class TodoListAdapter(
             itemCompleteClicked(todoData)
         }
 
-        if (todoData.completeTodoList) {
+        if (todoData.getBoolean("completeTodoList") == true) {
             holder.binding.itemTodoTextView.apply {
                 paintFlags = paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
                 setTypeface(null, Typeface.ITALIC)
@@ -52,5 +50,10 @@ class TodoListAdapter(
 
     override fun getItemCount(): Int {
         return todoListData.size
+    }
+
+    fun setData(newData: List<DocumentSnapshot>) {
+        todoListData = newData
+        notifyDataSetChanged()
     }
 }
