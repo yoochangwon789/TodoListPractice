8041e96a023859fe765f9bc4441bb89585204873
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2021-10-21 21:36:37 +0900

    Feat : Room databaseBuilder 초기 셋업

--- app/src/main/java/com/yoochangwonspro/todolistpractice/AppDatabase.kt
+++ app/src/main/java/com/yoochangwonspro/todolistpractice/AppDatabase.kt
@@ -1,10 +1,11 @@
 package com.yoochangwonspro.todolistpractice
 
 import androidx.room.Database
+import androidx.room.RoomDatabase
 import com.yoochangwonspro.todolistpractice.dao.TodoDetailModelDao
 import com.yoochangwonspro.todolistpractice.todomodel.TodoDetailModel
 
 @Database(entities = [TodoDetailModel::class], version = 1)
-abstract class AppDatabase {
+abstract class AppDatabase: RoomDatabase() {
     abstract fun todoDetailModelDao(): TodoDetailModelDao
 }
--- app/src/main/java/com/yoochangwonspro/todolistpractice/DetailItemActivity.kt
+++ app/src/main/java/com/yoochangwonspro/todolistpractice/DetailItemActivity.kt
@@ -2,17 +2,22 @@ package com.yoochangwonspro.todolistpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.room.Room
 import com.yoochangwonspro.todolistpractice.databinding.ActivityDetailItemBinding
 
 class DetailItemActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailItemBinding
+    private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailItemBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
+        db = Room.databaseBuilder(
+            this,
+            AppDatabase::class.java, "todoDetailRoom"
+        ).build()
     }
 }
